6aabfeb3968f4b1a7921c9837b7d63ecbf88cf97
alex8224 <alex8224@gmail.com>
2024-11-05 20:54:40 +0800

    support darwin

--- tray_windows.go
+++ tray_windows.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
-	"runtime"
 	"time"
 
 	"github.com/getlantern/systray"
@@ -25,28 +23,6 @@ func MakeTray() *tray {
 
 const trayToopTip string = "APIXIA客户端"
 
-func openDebugWin() {
-	url := fmt.Sprintf("http://localhost:%d/static/index.html", default_port)
-	switch runtime.GOOS {
-	case "windows":
-		err := exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-		if err != nil {
-			fmt.Printf("Error opening browser on Windows: %v\n", err)
-		}
-	case "darwin":
-		err := exec.Command("open", url).Start()
-		if err != nil {
-			fmt.Printf("Error opening browser on macOS: %v\n", err)
-		}
-	case "linux":
-		err := exec.Command("xdg-open", url).Start()
-		if err != nil {
-			fmt.Printf("Error opening browser on Linux: %v\n", err)
-		}
-	default:
-		fmt.Println("Unsupported platform", runtime.GOOS)
-	}
-}
 
 func (tray *tray) InitTray() {
 	systray.SetIcon(icon.Data)
