8b0281f62290f1b8bd547c272f9a3d83fa57ef5c
alex8224 <alex8224@gmail.com>
2024-11-05 21:05:21 +0800

    worflow for linux,windows

--- make.bat
+++ make.bat
@@ -4,12 +4,12 @@ setlocal
 if "%~1"=="linux" (
     set GOOS=linux
     set GOARCH=amd64
-    go clean && go build
+    go clean && go build -o dist/apixia-linux-amd64
 ) else (
      set GOOS=windows
      set GOARCH=amd64
      go clean
-     go build  -ldflags -H=windowsgui
+     go build  -ldflags -H=windowsgui -o dist/apixia-windows-amd64.exe
 )
 
 endlocal
